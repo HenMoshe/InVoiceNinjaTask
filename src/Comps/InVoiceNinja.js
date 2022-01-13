ce8291c1641b1d2b4a9cf6ff911670b841e0728f
HenMoshe <91905102+HenMoshe@users.noreply.github.com>
2022-01-13 23:15:36 +0200

    adding doc

--- src/Comps/InVoiceNinja.js
+++ src/Comps/InVoiceNinja.js
@@ -1,20 +1,20 @@
 import { useState } from 'react'
-import Header from "./Header";
+import DropSelection from "./DropSelection";
 import ListOfWords from './ListOfWords';
 import AddWord from './AddWord';
 import Note from './Note';
 function handleWords(index, addWords, addFunc, delFunc, delWords){
     let item = delWords.splice(index, 1)[0]
-    delFunc([...delWords])
+    delFunc([...delWords])                   /* the main function for moving words from selection drop to wordlist and backwards. */
     return addFunc([...addWords, item])
     }
 function InvoiceNinjaInterview(){
     let [selectionWords, setSelectionWords] = useState(['Client', 'Users', 'Technology'])
     let [listWords, setListWords] = useState(['Development', 'Invoice Ninja'])
   
-    return (
+    return (  /* the main state handling component. */
     <>
-        <Header handleWords={handleWords} delWords={selectionWords} addWords={listWords} delFunc={setSelectionWords} addFunc={setListWords}/>
+        <DropSelection handleWords={handleWords} delWords={selectionWords} addWords={listWords} delFunc={setSelectionWords} addFunc={setListWords}/>
         <hr/>
         <AddWord words={selectionWords} addWord={setSelectionWords}/>
         <hr/>
